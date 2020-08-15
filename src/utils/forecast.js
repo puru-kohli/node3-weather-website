f55e290ae395d1d7636ff6ac95620d096e421a57
puru <purukohli01@gmail.com>
2020-08-15 17:53:45 +0530

    adding humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
     }else if(response.body.error){
         callback('unable to find location', undefined)
     }else{
-        callback(undefined, response.body.current.weather_descriptions[0] + '. it is currently ' + response.body.current.temperature + ' degrees out. it feels like ' + response.body.current.feelslike + ' degrees out.')
+        callback(undefined, response.body.current.weather_descriptions[0] + '. it is currently ' + response.body.current.temperature + ' degrees out. it feels like ' + response.body.current.feelslike + ' degrees out. Humidty is ' + response.body.current.humidity)
     }
 })
 }
